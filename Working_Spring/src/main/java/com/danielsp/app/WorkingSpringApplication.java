d559eb0537257dd628fba7263f613f3c263b1f10
fuxionsp2403 <decora_04@hotmail.com>
2021-07-18 21:18:47 -0500

    dale Spring

---
+++ Working_Spring/src/main/java/com/danielsp/app/WorkingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.danielsp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingSpringApplication.class, args);
+	}
+
+}
---
+++ Working_Spring/src/test/java/com/danielsp/app/WorkingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielsp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
